4e3fcfc55b9aaf04ceb0b707fd77daf3bd69e0b1
yorche3 <hyaoki123@gmail.com>
2026-09-18 14:09:38 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/naive_sort.lua
+++ core/algorithms/naive_sort/src/naive_sort.lua
@@ -14,4 +14,54 @@
 
 local naive_sort = {}
 
+function naive_sort.selection_sort(arr)
+    if arr == nil then return nil end
+    local n = #arr
+    if n <= 1 then return arr end
+    for i = 1, n - 1 do
+        local min_index = i
+        for j = i + 1, n do
+            if arr[j] < arr[min_index] then
+                min_index = j
+            end
+        end
+        if min_index ~= i then
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+        end
+    end
+    return arr
+end
+
+function naive_sort.bubble_sort(arr)
+    if arr == nil then return nil end
+    local n = #arr
+    if n <= 1 then return arr end
+    for i = 1, n - 1 do
+        local swapped = false
+        for j = 1, n - i do
+            if arr[j] > arr[j + 1] then
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = true
+            end
+        end
+        if not swapped then break end
+    end
+    return arr
+end
+
+function naive_sort.insertion_sort(arr)
+    if arr == nil then return nil end
+    local n = #arr
+    if n <= 1 then return arr end
+    for i = 2, n do
+        local key = arr[i]
+        local j = i - 1
+        while j >= 1 and arr[j] > key do
+            arr[j + 1] = arr[j]
+            j = j - 1
+        end
+        arr[j + 1] = key
+    end
+    return arr
+end
 return naive_sort
